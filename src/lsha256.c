e06fca124a034f4076ac4ee84978cb7fc2c96c9a
adaxi <dev@adaxisoft.be>
2017-04-07 22:20:28 +0200

    M0 Imported Upstream version 0.0.1

---
+++ src/lsha256.c
@@ -0,0 +1,5 @@
+#define L_SHA_MODE 256
+#define L_SHA_MODE_BASE 256
+#include "lsha.inc"
+#undef L_SHA_MODE_BASE
+#undef L_SHA_MODE
---
+++ src/lsha512_192.c
@@ -0,0 +1,5 @@
+#define L_SHA_MODE 512_192
+#define L_SHA_MODE_BASE 512
+#include "lsha.inc"
+#undef L_SHA_MODE_BASE
+#undef L_SHA_MODE
